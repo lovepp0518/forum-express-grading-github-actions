6ddd301f9416d16fe9883c5930f57090c555e0eb
lovepp0518 <lovepp0518@gmail.com>
2024-01-09 16:19:21 +0800

    feat: category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -11,5 +11,6 @@ router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategories)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
