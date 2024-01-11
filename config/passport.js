3dc8e2fb5402cf3043ec7de9d89d1470e9de2f05
lovepp0518 <lovepp0518@gmail.com>
2024-01-12 00:07:57 +0800

    add timeout to 30000 and add log

--- config/passport.js
+++ config/passport.js
@@ -1,9 +1,9 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcryptjs')
-// const db = require('../models')
-// const User = db.User
-const { User } = require('../models')
+const db = require('../models')
+const User = db.User
+
 // set up Passport strategy
 passport.use(new LocalStrategy(
   // customize user field
@@ -17,12 +17,22 @@ passport.use(new LocalStrategy(
   (req, email, password, cb) => {
     User.findOne({ where: { email } })
       .then(user => {
+        console.log('log1:' + user)
         // 帳號及密碼錯誤顯示相同的錯誤訊息有助於資安防護
-        if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
-        bcrypt.compare(password, user.password).then(res => {
-          if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
-          return cb(null, user)
-        })
+        if (!user) {
+          console.log('log2:' + user)
+          return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+        }
+        bcrypt.compare(password, user.password)
+          .then(res => {
+            console.log('log3.1:' + user)
+            console.log('log3.2:' + res)
+            if (!res) {
+              console.log('log4:' + res)
+              return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+            }
+            return cb(null, user)
+          })
       })
 
       .catch(err => {
