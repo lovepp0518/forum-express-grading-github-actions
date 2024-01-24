9459966f2f3e3c9a894f6720aa497edf34ed936d
lovepp0518 <lovepp0518@gmail.com>
2024-01-25 02:09:26 +0800

    feat: finish R05

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -90,6 +90,23 @@ const restaurantController = {
         })
       })
       .catch(err => next(err))
+  },
+  getTopRestaurants: (req, res, next) => {
+    return Restaurant.findAll({
+      include: [{ model: User, as: 'FavoritedUsers' }]
+    })
+      .then(restaurants => {
+        const result = restaurants
+          .map(restaurant => ({
+            ...restaurant.toJSON(),
+            favoritedCount: restaurant.FavoritedUsers.length,
+            isFavorited: req.user && req.user.FavoritedRestaurants.some(f => f.id === restaurant.id) // req.user 可能是空值，這個表達式讓req.user為空值時能夠回傳false
+          }))
+          .sort((a, b) => b.favoritedCount - a.favoritedCount)
+          .slice(0, 10)
+        return res.render('top-restaurants', { restaurants: result })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
