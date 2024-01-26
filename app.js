2b210df7745e54ced7c278229613b5a7996e9a47
lovepp0518 <lovepp0518@gmail.com>
2024-01-26 11:30:22 +0800

    feat: add getRestaurants api

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const { getUser } = require('./helpers/auth-helpers')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
-const { pages } = require('./routes')
+const { pages, apis } = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
@@ -32,6 +32,7 @@ app.use((req, res, next) => {
   next()
 })
 
+app.use('/api', apis)
 app.use(pages)
 
 app.listen(port, () => {
